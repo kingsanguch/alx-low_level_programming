23bcc6390b260baa3fa9d1a0ac37f967e1db1561
kingsanguch <sangurajohnnie17@gmail.com>
2023-07-13 05:34:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,17 +12,10 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: %zu byte(s)\n", sizeof(a));
-printf("Size of an int: %zu byte(s)\n", sizeof(b));
-printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-printf("Size of a float: %zu byte(s)\n", sizeof(f));
-
-printf("[Expected]\n");
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Size of a float: 4 byte(s)\n");
+printf("Size of a char: %i byte(s)\n", sizeof(a));
+printf("Size of an int: %i byte(s)\n", sizeof(b));
+printf("Size of a long int: %i byte(s)\n", sizeof(c));
+printf("Size of a long long int: %i byte(s)\n", sizeof(d));
+printf("Size of a float: %i byte(s)\n", sizeof(f));
 return (0);
 }
