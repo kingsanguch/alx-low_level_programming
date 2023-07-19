b7d971509d4781f70eb955362162603aa9550c9f
kingsanguch <sangurajohnnie17@gmail.com>
2023-07-19 08:12:02 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,7 @@ unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
-printf("%lu,", sum);
+printf("%lu, ", sum);
 fib1 = fib2;
 fib2 = sum;
 }
@@ -36,7 +36,7 @@ half2 %= 10000000000;
 }
 printf("%lu%lu", half1, half2);
 if (count != 98)
-printf(",");
+printf(", ");
 fib1_half1 = fib2_half1;
 fib1_half2 = fib2_half2;
 fib2_half1 = half1;
