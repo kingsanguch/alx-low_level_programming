f5c110a2c72a598c98ac99ed4bcf5074b7adfdfe
kingsanguch <sangurajohnnie17@gmail.com>
2023-07-19 07:20:42 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,14 +15,14 @@ long j = 1, k = 2;
 while (i < 50)
 {
 if (i == 0)
-printf("%d", j);
+printf("%ld", j);
 else if (i == 1)
-printf(", %d", k);
+printf(", %ld", k);
 else
 {
 k += j;
 j = k - j;
-printf(", %d", k);
+printf(", %ld", k);
 }
 ++i;
 }
