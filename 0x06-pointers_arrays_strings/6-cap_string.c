75548054eb5fad9049fe74481cf6daacc9b39508
kingsanguch <sangurajohnnie17@gmail.com>
2023-07-26 10:33:34 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+/**
+ * cap_string - capitalizes the first letter of each word in a string
+ * @s: pointer to string
+ * Return: pointer to s.
+ */
+char *cap_string(char *s)
+{
+int string_count;
+
+/* Scan through the string */
+string_count = 0;
+while (s[string_count] != '\0')
+{
+if (s[0] >= 97 && s[0] <= 122)
+{
+s[0] = s[0] - 32;
+}
+if (s[string_count] == ' ' || s[string_count] == '\t'
+|| s[string_count] == '\n'
+|| s[string_count] == '.' || s[string_count] == ':' || s[string_count] == '!'
+|| s[string_count] == '?' || s[string_count] == '"' || s[string_count] == '('
+|| s[string_count] == ')' || s[string_count] == '{' || s[string_count] == '}')
+{
+if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+{
+s[string_count + 1] = s[string_count + 1] - 32;
+}
+}
+string_count++;
+}
+return (s);
+}
