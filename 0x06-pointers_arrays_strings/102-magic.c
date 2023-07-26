ca592d30a62dd6b6bcf698090859c5bfe6bca326
kingsanguch <sangurajohnnie17@gmail.com>
2023-07-26 12:11:07 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - main code
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Description: The main function demonstrates pointer arithmetic to modify
+ *              the value of a[2] to 98 without directly using the variable a.
+ *              It uses a pointer p to point to an integer n, and by performing
+ *            pointer arithmetic on p, it accesses modifies the value of a[2].
+ *              The modified value of a[2] is then printed.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 int n;
-int a[5];
 int *p;
 
-a[2] = 1024;
 p = &n;
-*(p + 5) = 98;
-printf("a[2] = %d\n", a[2]);
+*(p + 5) = 98;  /* New line of code to set a[2] to 98 */
+printf("a[2] = %d\n", *(p + 5));
 return (0);
 }
+
