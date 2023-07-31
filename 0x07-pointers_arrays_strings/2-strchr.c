71dc509f5420bf2062c05214d511f8c6a39498dd
kingsanguch <sangurajohnnie17@gmail.com>
2023-07-31 07:16:50 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
+	int index;
 
-while (*s != '\0')
-{
-if (*s == c)
-{
-return (s);
-}
-s++;
-}
-
-return ('\0');
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
 }
