d427e1821443e3b0e7f39e9ed88731bf7e00f12e
kingsanguch <sangurajohnnie17@gmail.com>
2023-08-04 09:29:54 -0700

    updated

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,14 +2,15 @@
 
 /**
  * _puts - prints string followed by new line, to stdout
- * @str; string to print
+ * @str: string to print
  *
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	char *current = str;
+
+	while (*current != '\0')
 	{
-		_putchar(*str++);
+		current++;
 	}
-	_putchar('\n');
 }
