636bc74685cfcfd6167dcc8b14cad0fbee9ec64d
kingsanguch <sangurajohnnie17@gmail.com>
2023-08-04 15:10:12 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: count argument
+ * @argv: arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+(void argv; /*ignore argv*/
+printf("i\n", argc - 1);
+return (0);
+}
