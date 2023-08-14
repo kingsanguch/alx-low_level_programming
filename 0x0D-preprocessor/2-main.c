4b70e4246cb3925a119ad76eb950cb996ce131ba
kingsanguch <sangurajohnnie17@gmail.com>
2023-08-14 08:15:44 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+/* Print the name of the current source file */
+printf("%s\n", __FILE__);
+
+return 0;
+}
