d8b7c7b4196a90d90b1d7f076962c39b3a4742eb
kingsanguch <sangurajohnnie17@gmail.com>
2023-08-24 15:43:31 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,19 @@
 section .data
-    hello db "Hello, Holberton", 10 ; The 10 represents a new line character
+hello db 'Hello, Holberton',10   ; The 10 is for a newline character
+hello_len equ $ - hello
 
 section .text
-    global _start
+global _start
 
 _start:
-    ; Write "Hello, Holberton" to stdout (file descriptor 1)
-    mov rax, 1             ; syscall number for sys_write
-    mov rdi, 1             ; file descriptor 1 (stdout)
-    mov rsi, hello         ; pointer to the string
-    mov rdx, 16            ; string length
-    syscall
+; Write "Hello, Holberton" to stdout (file descriptor 1)
+mov eax, 4           ; syscall number for sys_write
+mov ebx, 1           ; file descriptor 1 (stdout)
+mov ecx, hello       ; pointer to the string
+mov edx, hello_len   ; string length
+int 0x80             ; invoke syscall
 
-    ; Exit the program
-    mov rax, 60            ; syscall number for sys_exit
-    xor rdi, rdi           ; exit status 0
-    syscall
-
+; Exit the program
+mov eax, 1           ; syscall number for sys_exit
+xor ebx, ebx         ; exit status 0
+int 0x80             ; invoke syscall
